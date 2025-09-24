43dc05a72d77cfdc3650b1dda72e520022748ae8
Илья <iiyazxcv@icloud.com>
2025-09-24 22:10:36 +0600

    3 step

---
+++ src/app/app.tsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+import { AppHeader } from "@/features/header";
+
+
+
+export function App() {
+    return (
+        <div>
+            <AppHeader />
+            <Outlet />
+        </div>
+    );
+}
