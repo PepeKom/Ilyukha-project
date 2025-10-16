35330ebf8716295ef7bdeea9d241334d6cbf0060
Илья <iiyazxcv@icloud.com>
2025-10-16 17:16:31 +0600

    9 step video , the last step of the first part of the video

---
+++ src/features/board/view-model/view-model-params.ts
@@ -0,0 +1,16 @@
+import type { Dispatch } from "react";
+import type { CanvasRect } from "../hooks/use-canvas-rect";
+import type { NodesModel } from "../model/nodes";
+
+import type { SetStateAction } from "react";
+import type { ViewState } from "./use-view-model";
+import type { NodesDimensionsMap } from "../hooks/use-nodes-dimensions";
+import type { WindowPositionModel } from "../model/window-position";
+
+export type ViewModelParams = {
+    setViewState: Dispatch<SetStateAction<ViewState>>;
+    nodesModel: NodesModel;
+    canvasRect: CanvasRect | undefined;
+    nodesDimensions: NodesDimensionsMap;
+    windowPositionModel: WindowPositionModel;
+};
--- src/features/header/index.tsx
+++ src/features/header/index.tsx
@@ -8,7 +8,7 @@ export function AppHeader() {
   return (
       <header className="bg-background border-b border-border/40 shadow-sm py-3 px-4 mb-6">
         <div className="max-w-7xl mx-auto flex items-center justify-between">
-          <div className="text-xl font-semibold">Miro Copy</div>
+          <div className="text-xl font-semibold">koks</div>
 
           {session ? (
               <div className="flex items-center gap-4">
